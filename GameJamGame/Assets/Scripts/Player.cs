36a6010269792b7d251fa1e2d5280833c7aeb494
Michael <mickmick117@hotmail.com>
2018-03-24 01:41:03 -0500

    asdf

---
+++ GameJamGame/Assets/Scripts/Player.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public float Speed = 1;
+    public float JumpHeight = 1;
+
+    private Rigidbody2D myRigidbody;
+    private bool facingRight;
+    private Animator myAnimator;
+
+    // Use this for initialization
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        HandleInput();
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        HandleMovement(horizontal);
+        Flip(horizontal);
+        RestValues();
+    }
+
+    private void HandleMovement(float horizontal)
+    {
+        myRigidbody.velocity = new Vector2(horizontal * Speed, myRigidbody.velocity.y);
+        myAnimator.SetFloat("speed", Mathf.Abs(horizontal));
+    }
+
+    private void HandleInput()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            myRigidbody.velocity = new Vector3(0, 10 * JumpHeight * Time.deltaTime, 0);
+        }
+    }
+
+    private void Flip(float horizontal)
+    {
+        if (horizontal < 0 && !facingRight || horizontal > 0 && facingRight)
+        {
+            facingRight = !facingRight;
+            Vector3 scale = transform.localScale;
+            scale.x *= -1;
+            transform.localScale = scale;
+        }
+    }
+
+    private void RestValues()
+    {
+
+    }
+}
