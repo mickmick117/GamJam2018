64542e6ef9fc19de02da5d46d2985997448ce037
raphaeldoye <raphaeldoye@hotmail.fr>
2018-03-23 23:06:44 -0400

    auto generate level

--- GameJamGame/Assets/Scripts/LevelGenerator.cs
+++ GameJamGame/Assets/Scripts/LevelGenerator.cs
@@ -6,21 +6,37 @@ public class LevelGenerator : MonoBehaviour {
 
    public GameObject Ground;
 
-    int nbOfGround;
+    int maxGround;
     int maxLength;
     int minLength;
     int maxHeigth;
+    int minHeigth;
     int currentLevel = 1;
+    int groundIteration;
 
 
 	// Use this for initialization
 	void Start () {
-        nbOfGround = 100;
+        maxGround = 100;
         maxLength = 8;
         minLength = 1;
         maxHeigth = 3;
+        minHeigth = -3;
+        groundIteration = 1;
 
         Instantiate(Ground, new Vector2(0, 0), Quaternion.identity);
+
+        for (int i = 1; i < maxGround; i++)
+        {
+            int nbGround = Mathf.RoundToInt(Random.Range(minLength, maxLength));
+            int heigth = Mathf.RoundToInt(Random.Range(minHeigth, maxHeigth));
+
+            for (int j = 0; j < nbGround; j++)
+            {
+                Instantiate(Ground, new Vector2(groundIteration, heigth), Quaternion.identity);
+                groundIteration++;
+            }
+        }
     }
 	
 	// Update is called once per frame
